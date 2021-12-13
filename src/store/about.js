141edcd1c7aa20e8d08945b421f97c5105f5f76f
hyunwoo045 <hyunwoo045@gmail.com>
2021-12-13 14:52:27 +0900

    재구성

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: "BOOOANG",
+    email: "hyunwoo045@gmail.com",
+    blog: "https://hyunwoo045.github.io/",
+    phone: "+82-10-1234-4321",
+    image: "https://heropy.blog/css/images/logo.png",
+  }),
+};
---
+++ src/store/movie.js
@@ -0,0 +1,125 @@
+import axios from "axios";
+import _uniqBy from "lodash/uniqBy";
+
+const _defaultMessage = "영화 제목을 검색해 보세요!";
+
+export default {
+  // export 되어지는 해당 코드가 store 의 모듈로 만들 것인지에 대한 여부.
+  // true 라고 넣어줌으로써 ~/store/index.js 에서 module 에 추가해 줄 수 있음.
+  namespaced: true,
+
+  state: () => ({
+    movies: [],
+    message: _defaultMessage,
+    loading: false,
+    theMovie: {},
+  }),
+  getters: {},
+
+  // mutations, actions 모두 methods 의 정의이다
+  // mutations 는 state 를 수정할 수 있다.
+  // actions 는 state 를 수정할 수 없다.
+  // 따로 구분지어 둠으로써 데이터 관리의 복잡성을 줄여 준다.
+  mutations: {
+    updateState(state, payload) {
+      Object.keys(payload).forEach((key) => {
+        state[key] = payload[key];
+      });
+    },
+    resetMovies(state) {
+      state.movies = [];
+      state.message = _defaultMessage;
+      state.loading = false;
+    },
+  },
+  actions: {
+    async searchMovies({ state, commit }, payload) {
+      if (state.loading) return;
+
+      commit("updateState", {
+        message: "",
+        loading: true,
+      });
+      try {
+        const res = await _fetchMovie({
+          ...payload,
+          page: 1,
+        });
+        const { Search, totalResults } = res.data;
+        commit("updateState", {
+          movies: _uniqBy(Search, "imdbID"),
+        });
+
+        const total = parseInt(totalResults, 10);
+        const pageLength = Math.ceil(total / 10);
+
+        if (pageLength > 1) {
+          for (let page = 2; page <= pageLength; page += 1) {
+            if (page > payload.number / 10) break;
+            const res = await _fetchMovie({
+              ...payload,
+              page,
+            });
+
+            const { Search } = res.data;
+            commit("updateState", {
+              movies: [...state.movies, ..._uniqBy(Search, "imdbID")],
+            });
+          }
+        }
+      } catch (err) {
+        commit("updateState", {
+          movies: [],
+          message: err,
+        });
+      } finally {
+        commit("updateState", {
+          loading: false,
+        });
+      }
+    },
+    async searchMovieWithId({ state, commit }, payload) {
+      if (state.loading) return;
+
+      commit("updateState", {
+        theMovie: {},
+        loading: true,
+      });
+
+      try {
+        const res = await _fetchMovie(payload);
+        commit("updateState", {
+          theMovie: res.data,
+        });
+      } catch (err) {
+        commit("updateState", {
+          theMovie: {},
+        });
+      } finally {
+        commit("updateState", {
+          loading: false,
+        });
+      }
+    },
+  },
+};
+
+function _fetchMovie(payload) {
+  const { title, type, year, page, id } = payload;
+  const OMDB_API_KEY = "8b5d0b97";
+  const url = id
+    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
+    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`;
+
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url)
+      .then((res) => {
+        if (res.data.Error) reject(res.data.Error);
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err.message);
+      });
+  });
+}
